f1dbc413364e8208af06445577f240e0e7fbf1d4
Ehsan Akhavan <ehsan.akhavan@yahoo.com>
2023-09-09 18:51:28 +0330

    remove wlsb ack and sn

--- sources/rohc_comp/comp_wlsb.h
+++ sources/rohc_comp/comp_wlsb.h
@@ -128,13 +128,5 @@ size_t wlsb_get_kp_32bits(const struct c_wlsb *const wlsb,
                           const rohc_lsb_shift_t p)
 	__attribute__((warn_unused_result, nonnull(1)));
 
-size_t wlsb_ack(struct c_wlsb *const wlsb,
-                const uint32_t sn_bits,
-                const size_t sn_bits_nr)
-	__attribute__((warn_unused_result, nonnull(1)));
-
-bool wlsb_is_sn_present(struct c_wlsb *const wlsb, const uint32_t sn)
-	__attribute__((warn_unused_result, nonnull(1)));
-
 #endif
 
